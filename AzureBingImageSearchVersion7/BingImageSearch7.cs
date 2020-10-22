1c1e23cd06562d696b82a4a9e9e2d478291ae8a2
beyanon <tokabe333@gmail.com>
2020-10-22 12:49:37 +0900

    update : Nuget JSONを追加

--- AzureBingImageSearchVersion7/BingImageSearch7.cs
+++ AzureBingImageSearchVersion7/BingImageSearch7.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json.Linq;
 
 namespace AzureBingImageSearchVersion7 {
     class BingImageSearch7 {
+
     } //End_Class
 } //End_Namespace
 
--- AzureBingImageSearchVersion7/Program.cs
+++ AzureBingImageSearchVersion7/Program.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 namespace AzureBingImageSearchVersion7 {
     class Program {
         static void Main(string[] args) {
-        }
-    }
-} 
+            var apikey = args[0];
+            var url = args[1];
+        } // End_Methods
+    } // End_Class
+} // End_Namespace
