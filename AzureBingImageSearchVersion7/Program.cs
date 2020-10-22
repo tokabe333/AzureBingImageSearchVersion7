a626ae3f50ddab1981c88166f1169965e827a78e
beyanon <tokabe333@gmail.com>
2020-10-22 14:16:09 +0900

    update : コマンドライン引数解析良好

--- AzureBingImageSearchVersion7/Program.cs
+++ AzureBingImageSearchVersion7/Program.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 namespace AzureBingImageSearchVersion7 {
 	class Program {
 		static void Main(string[] args) {
+			var commandOption = CommandLine.Parser.Default.ParseArguments<Options>(args) as CommandLine.Parsed<Options>;
+
 			var apikey = args[0];
 			var dir = args[1];
 			var term = args[2];
@@ -14,4 +16,15 @@ namespace AzureBingImageSearchVersion7 {
 			bing.StartSearchAndDownload();
 		} // End_Methods
 	} // End_Class
+
+	class Options {
+		[CommandLine.Option('k', "key", HelpText = "AzureBingImageSearchのAPIキー")]
+		public string ApiKey { get; set; }
+
+		[CommandLine.Option('d', "dir", HelpText = "保存先ディレクトリ(この直下に検索名でディレクトリ作成")]
+		public string SaveDir { get; set; }
+
+		[CommandLine.Option('t', "term", HelpText = "検索したい画像名")]
+		public string SearchTerm { get; set; }
+	} //End_Class
 } // End_Namespace
