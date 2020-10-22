e3e003f582352928e471ea16d6af89e8ad210c92
beyanon <tokabe333@gmail.com>
2020-10-22 13:05:30 +0900

    update : request作成

--- AzureBingImageSearchVersion7/BingImageSearch7.cs
+++ AzureBingImageSearchVersion7/BingImageSearch7.cs
@@ -8,8 +8,48 @@ using System.Net;
 using Newtonsoft.Json.Linq;
 
 namespace AzureBingImageSearchVersion7 {
-    class BingImageSearch7 {
+	class BingImageSearch7 {
+		private const string UriBase = "https://api.cognitive.microsoft.com/bing/v7.0/images/search";
+		private string SearchTerm = null;
+		private string ApiKey = null;
 
-    } //End_Class
+		public struct SearchResult {
+			public string JsonResult;
+			public Dictionary<string, string> RelevantHeaders;
+		} //End_Struct
+		
+
+		public BingImageSearch7(string apiKey, string searchTerm) {
+			this.ApiKey = apiKey;
+			this.SearchTerm = searchTerm;
+		} //End_Constructor
+		
+		public bool StartSearchAndDownload() {
+			return true;
+		} //End_Method
+
+		public SearchResult BingImageSearch() {
+			// クエリを生成してリクエストしてJSONレスポンスを得る
+			var query = BingImageSearch7.UriBase + "?q=" + Uri.EscapeDataString(this.SearchTerm);
+			var request = WebRequest.Create(query);
+			request.Headers["Ocp-Apim-Subscription-Key"] = this.ApiKey;
+			var response = (HttpWebResponse)request.GetResponseAsync().Result;
+			var json = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+			// JSONを解析して返す
+			SearchResult result = new SearchResult() {
+				JsonResult = json,
+				RelevantHeaders = new Dictionary<string, string>()
+			};
+			foreach(string header in response.Headers) {
+				if (header.StartsWith("BingAPIs-") || header.StartsWith("X-MSEdge-")) {
+					result.RelevantHeaders[header] = response.Headers[header];
+				} // End_If
+			} // End_Foreach
+
+			return result;
+		} //End_Method
+		
+	} //End_Class
 } //End_Namespace
 
--- AzureBingImageSearchVersion7/Program.cs
+++ AzureBingImageSearchVersion7/Program.cs
@@ -5,10 +5,10 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace AzureBingImageSearchVersion7 {
-    class Program {
-        static void Main(string[] args) {
-            var apikey = args[0];
-            var url = args[1];
-        } // End_Methods
-    } // End_Class
+	class Program {
+		static void Main(string[] args) {
+			var apikey = args[0];
+			var url = args[1];
+		} // End_Methods
+	} // End_Class
 } // End_Namespace
